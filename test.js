4b5d014fe0b76db09fe093673bea6ade83424a83
GhislainFaure <ghislainfaure06@gmail.com>
2023-12-10 12:30:07 +0100

    test_git

--- test.js
+++ test.js
@@ -1,36 +1,29 @@
 // on va charger nos variables d'environnement
-require('dotenv').config();
+require("dotenv").config();
 
-const { List, Card, Tag } = require('./app/models');
+const { List, Card, Tag } = require("./app/models");
 
 async function test() {
-// essayons de récuperer les listes
+  // essayons de récuperer les listes
 
-const listArray = await List.findAll({
-    include: [{
-        association: 'cards',
-        include:[
-            'tags'
-        ]
+  const listArray = await List.findAll({
+    include: [
+      {
+        association: "cards",
+        include: ["tags"],
+      },
+    ],
+  });
+  console.log("Voici les listes :", listArray);
+  for (const list of listArray) {
+    for (const card of list.cards) {
+      console.log("j'ai une carte : ", card.content);
+
+      for (const tag of card.tags) {
+        console.log("un tag de la carte : ", tag.name);
+      }
     }
-    ]
-});
-console.log('Voici les listes :', listArray);
-for (const list of listArray) {
-
-    for (const card of list.cards){
-        console.log("j'ai une carte : ", card.content );
-
-        for (const tag of card.tags) {
-            console.log('un tag de la carte : ', tag.name);
-        }
-
-    }
-
+  }
 }
 
-};
-
 test();
-
-
